67d251e45b9489960e8e35cd500f0d6bbb4db949
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-01 21:09:19 +0545

    Admin Panel

---
+++ ecomm/admin/home.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musical Instrument</title>
+    <link rel="stylesheet" href="home.css" type="text/css">
+    <script src="https://kit.fontawesome.com/0280874e71.js" crossorigin="anonymous"></script>
+
+<style>
+    .product-nav
+{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #8b4513;
+    padding: 10px;
+    /* margin: 10px, 50px, 10px,50px; */
+    margin-top: 10px;
+    margin-left: 100px;
+    margin-right: 100px;
+    
+
+}
+    .product-nav a{
+    /* display: block; */
+    color: white;
+    text-align: center;
+    padding: 14px 20px;
+    text-decoration: none;
+
+}
+.product-nav a:hover{
+    background-color: #6b4226;
+    
+}
+</style>
+</head>
+<body>
+    <div>
+        <header>
+            <h1>Musical Instrument Store</h1>    
+    </header>
+        
+            <!-- <div class="logo"> -->
+                
+                <nav>
+                <div class="logo">
+                <a href="">LOGO</a>
+                </div>
+                
+                <ul>
+                    <li><a href="index.html">HOME</a></li> 
+                        <li><a href="">SHOP</a></li>
+                        <li><a href="">DEPARTMENT</a></li>
+                        <li><a href="">BRAND</a></li>
+                        <!-- <li><a href="">Rentals</a></li> -->
+                        <li><a href="">ABOUT US</a></li>
+                    </ul>
+                
+                <!-- <div class="searchBar">
+                    <input type="text" placeholder="Search for instruments" id="search">
+                    <button onclick="searchInstrument()">Search</button> -->
+
+                <div class="login">
+                   <!-- <div class="searchcart"><input type="text" placeholder="Search instruments"></div> -->
+                   <!-- <button class="btn">🔍</button> 
+                    <button class="btn">🛒</button> -->
+            <i class="fa-solid fa-user-tie ">  Admin </i>
+
+                    <!-- Admin |  -->
+
+                    <a href="signUp.html">UserPanel</a>
+            <i class="fa-solid fa-right-from-bracket"></i>
+
+                    <a href="login.html">LOG out</a>
+                    <a href=""></a>
+                </div>
+            </nav>
+            </div>
+
+
+            <div class="product-nav">
+                <a href="product/index.php">Add Products</a>
+                <a href="">Users</a>
+            </div>
+        
+        
+    </div>
+</body>
+</html>
