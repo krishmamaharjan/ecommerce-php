d0ba446dc5c460956f1f5858923c172cbef606f8
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-05 18:52:40 +0545

    header/navbar added

---
+++ ecomm/header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musical Instrument</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="https://kit.fontawesome.com/0280874e71.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div>
+        <header>
+            <h1>Musical Instrument Store</h1>    
+    </header>
+        
+            <!-- <div class="logo"> -->
+                
+                <nav>
+                <div class="logo">
+                <a href="">LOGO</a>
+                </div>
+                
+                    <ul>
+                    <li><a href="index.html">HOME</a></li> 
+                        <li><a href="">SHOP</a></li>
+                        <li><a href="">DEPARTMENT</a></li>
+                        <li><a href="">BRAND</a></li>
+                        <!-- <li><a href="">Rentals</a></li> -->
+                        <li><a href="">ABOUT US</a></li>
+                    </ul>
+                
+                <!-- <div class="searchBar">
+                    <input type="text" placeholder="Search for instruments" id="search">
+                    <button onclick="searchInstrument()">Search</button> -->
+
+                <div class="login">
+
+                   <!-- <div class="searchcart"><input type="text" placeholder="Search instruments"></div> -->
+                   <!-- <button class="btn">🔍</button>  -->
+                    <i class="fa-solid fa-cart-arrow-down"></i> 
+
+                    <a href="signUp.html">SIGN UP</a>
+                    <a href="login.html">LOG IN</a>
+                    <i class="fa-solid fa-circle-user"></i>
+                </div>
+            </nav>
+            </div>
+        
+        
+    </div>
+
+
+
+    <!-- <div class="slider">
+        <button class="prev" onclick="prevSlide()">&#10094;</button>
+        <div class="slides">
+            <div class="slide active"><img src="images/DrumSet.jpg" alt=""></div>
+            <div class="slide">Slide 2 Content <img src="images/guitar.webp" alt=""></div>
+            <div class="slide">Slide 3 Content</div>
+        </div>
+        <button class="next" onclick="nextSlide()">&#10095;</button>
+    </div>
+
+    <div class="dots">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div> -->
+
+    <!-- <script src="script.js"></script> -->
+
+    <!-- <section class="categories">
+        <h2>Browse By Category</h2>
+        <div class="category-container">
+            <div class="category-card">Category 1</div>
+            <div class="category-card">Category 2</div>
+            <div class="category-card">Category 3</div>
+            <div class="category-card">Category 4</div>
+            <div class="category-card">Category 5</div>
+        </div>
+    </section> -->
+
+
+    <!-- <div class="container" id="shop">
+        <div class="product">
+            <img src="images/guitar.webp" alt="Guitar">
+            <h2>Acoustic Guitar</h2>
+            <p>Price: $200</p>
+            <button>Buy Now</button>
+            <button>Rent</button>
+        </div>
+    </div> -->
+
+
+
+
+
+
+
+
+</body>
+</html>
