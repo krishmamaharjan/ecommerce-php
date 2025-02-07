974138408cf0895e84115d78bcd46e5c07b20e9b
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-07 20:43:37 +0545

    home page

--- ecomm/index.php
+++ ecomm/index.php
@@ -7,34 +7,88 @@
 
     <title>Home-page</title>
     <?php
-    include 'header.php';
+    include 'include/header.php';
     ?>
+
+    <style>
+        .section{
+            text-align: center;
+            margin: 50px auto;
+            width: 80%;
+        }
+       .section h3
+        {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+        
+
+
+    </style>
 </head>
 <body>
+    
+<!-- <div class="new-arrival"> -->
     <?php
-        include 'connection.config.php';
-        $record = mysqli_query($con, "SELECT * FROM product");
-            while($row = mysqli_fetch_array($record))
-            {
-                echo "
-                <div class='container' id='shop'>
-                        <div class='product'>
-                            <img src='admin/product/$row[Pimage]' alt='Guitar'>
-                        <h2>$row[Pname]</h2>
-                            <p>Price: $row[Pprice]</p>
-                            <button>Buy Now</button>
-                            
-                        </div>
-                    </div> 
+        // include 'connection.config.php';
+        // $record = mysqli_query($con, "SELECT * FROM product");
+        //     while($row = mysqli_fetch_array($record))
+        //     {
+        //         $check_page = $row['pages'];
+        //         if($check_page === 'Home')
+        //         {
+
+                
+                // <a href=""></a>
 
 
-                ";
+                    // echo "
 
-                echo "Image Path: admin/product/.$row[Pimage]";
+                    // <a href='BuyProduct.php'>
+                    // <div class='container' id='shop'>
 
-            }
+                    // <form action = 'InsertCart' method = 'POST' >
+                    //         <div class='product'>
+                    //             <img src='admin/product/$row[Pimage]' alt='Guitar'>
+                    //         <h2>$row[Pname]</h2>
+                    //             <p>Price: $row[Pprice]</p>
+                                
+                    //             <input type='submit' name='submit'class='btn' value='Add to cart'>
+                                
+
+                                
+                    //         </div>
+                    // </form>
+                    // </div> 
+                    // </a>
+
+
+                    // ";
+
+                    // echo "Image Path: admin/product/.$row[Pimage]";
+                // }
+
+            // }
                 
 
     ?>
+</div>
+
+
+
+
+<div class="section new-arrivals">
+    <h3>New Arrivals</h3>
+</div>
+
+<div class="section best-selling-product">
+    <h3>Best Selling Products</h3>
+</div>
+
+<div class="section browse-by-category">
+    <h3>Browse Categories</h3>
+</div>
+
+
 </body>
 </html>
