a43f6ce2cde7887679475ffbd1b21317f9189ab7
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-10 21:41:20 +0545

    insert-cart

--- ecomm/InsertCart.php
+++ ecomm/InsertCart.php
@@ -6,80 +6,88 @@ if (!isset($_SESSION['cart'])) {
   $_SESSION['cart'] = [];
 }
 
-if(isset($_POST['addCart']))
+if(isset($_SESSION['username']))
 {
 
-
-  $product_name = $_POST['Pname'];
-  $product_price = $_POST['Pprice'];
-  $product_quantity = $_POST['Pquantity'];
-
-
-  $check_product = array_column($_SESSION['cart'],'productName');
-  if(in_array($product_name,$check_product))
-  {
-    echo "
-    <script>
-      alert('product already added');
-      window.location.href = 'index.php';
-    </script>
-    ";
-  }
-  else
-  {
-    $_SESSION['cart'][] = array('productName' => $product_name, 
-                            'productPrice'=> $product_price, 
-                            'productQuantity'=>$product_quantity  );
-                            header("location:viewCart.php");
-                            exit();
-  // print_r($_SESSION['cart']);
-  }
-
-  
-  
-}
-
-
-
-// remove product
-
-if(isset($_POST['remove']))
-{
-  foreach($_SESSION['cart'] as $key => $value)
-  {
-    if($value['productName'] === $_POST['item'])
+    if(isset($_POST['addCart']))
     {
-      unset($_SESSION['cart'][$key]);
-      $_SESSION['cart'] = array_values($_SESSION['cart']);
-      header("location:viewCart.php");
-      exit();
+
+
+      $product_name = $_POST['Pname'];
+      $product_price = $_POST['Pprice'];
+      $product_quantity = $_POST['Pquantity'];
+
+
+      $check_product = array_column($_SESSION['cart'],'productName');
+      if(in_array($product_name,$check_product))
+      {
+        echo "
+        <script>
+          alert('product already added');
+          window.location.href = 'index.php';
+        </script>
+        ";
+      }
+      else
+      {
+        $_SESSION['cart'][] = array('productName' => $product_name, 
+                                'productPrice'=> $product_price, 
+                                'productQuantity'=>$product_quantity  );
+                                header("location:viewCart.php");
+                                exit();
+      // print_r($_SESSION['cart']);
+      }
+
+      
+      
     }
-  }
-}
 
 
 
+    // remove product
 
-// Update product
-
-if(isset($_POST['update']))
-{
-
-  $product_name = $_POST['Pname'];
-  $product_price = $_POST['Pprice'];
-  $product_quantity = $_POST['Pquantity'];
-
-  foreach($_SESSION['cart'] as $key => $value)
-  {
-    if($value['productName'] === $_POST['item'])
+    if(isset($_POST['remove']))
     {
-      $_SESSION['cart'][$key] = array('productName' => $product_name, 
-                                    'productPrice'=> $product_price, 
-                                    'productQuantity'=>$product_quantity  
-                                  ); 
-                                    header("location:viewCart.php");
-                                    exit();
+      foreach($_SESSION['cart'] as $key => $value)
+      {
+        if($value['productName'] === $_POST['item'])
+        {
+          unset($_SESSION['cart'][$key]);
+          $_SESSION['cart'] = array_values($_SESSION['cart']);
+          header("location:viewCart.php");
+          exit();
+        }
+      }
     }
-  }
+
+
+
+
+    // Update product
+
+    if(isset($_POST['update']))
+    {
+
+      $product_name = $_POST['Pname'];
+      $product_price = $_POST['Pprice'];
+      $product_quantity = $_POST['Pquantity'];
+
+      foreach($_SESSION['cart'] as $key => $value)
+      {
+        if($value['productName'] === $_POST['item'])
+        {
+          $_SESSION['cart'][$key] = array('productName' => $product_name, 
+                                        'productPrice'=> $product_price, 
+                                        'productQuantity'=>$product_quantity  
+                                      ); 
+                                        header("location:viewCart.php");
+                                        exit();
+        }
+      }
+    }
+}
+else
+{
+  header("location:login.php");
 }
 ?>
