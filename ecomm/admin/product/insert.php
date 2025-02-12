791c168c1ead08d58d0486b852f73781d2b27e00
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-12 17:32:39 +0545

    admin insert product

--- ecomm/admin/product/insert.php
+++ ecomm/admin/product/insert.php
@@ -7,6 +7,8 @@ if(isset($_POST['submit']))
     $product_price = $_POST['Pprice'];
 // print_r($_FILES);
     $product_image = $_FILES['Pimage'];
+    $product_detail = $_POST['Pdetail'];
+
 
     // print_r($product_image);
 
@@ -20,32 +22,12 @@ if(isset($_POST['submit']))
 
     // insert product 
 
-    mysqli_query($con, "INSERT INTO `product`(`Pname`, `Pprice`, `Pimage`, `Category`) VALUES('$product_name','$product_price','$img_des','$product_category')" );
+    $query = mysqli_query($con, "INSERT INTO `product`(`Pname`, `Pprice`, `Pimage`, `Category`, `Pdetail`) VALUES('$product_name','$product_price','$img_des','$product_category','$product_detail')" );
 
-    
-    
-    if(isset($_POST['submit'])) {
-    include 'connection.config.php';
-
-    $product_name = $_POST['Pname'];
-    $product_price = $_POST['Pprice'];
-    $product_image = $_FILES['Pimage'];
-
-    $image_location = $_FILES['Pimage']['tmp_name'];
-    $image_name = $_FILES['Pimage']['name'];
-
-    $img_des = "UploadImage/" . $image_name;
-    move_uploaded_file($image_location, $img_des);
-
-    $product_category = $_POST['Pages'];
-
-    // Insert product into the database
-    $query = "INSERT INTO `product`(`Pname`, `Pprice`, `Pimage`, `Category`) VALUES('$product_name','$product_price','$img_des','$product_category')";
-
-    if (mysqli_query($con, $query)) {
+    if ($query) {
         echo "<script>
                 alert('File uploaded successfully!');
-                window.location.href = 'index.php'; // Reload the main page
+                window.location.href = 'index.product.php'; // Reload the main page
               </script>";
     } else {
         echo "<script>alert('Database error: Unable to save product.');</script>";
@@ -53,5 +35,5 @@ if(isset($_POST['submit']))
     
 
 }
-}
+
 ?>
