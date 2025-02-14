c164cd5a08e516ecab3e8bd560ccb08bf39f8c83
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-14 14:16:18 +0545

    admin-delete-product

--- ecomm/admin/product/delete.php
+++ ecomm/admin/product/delete.php
@@ -6,6 +6,6 @@
  include 'connection.config.php';
 
  mysqli_query($con, "DELETE FROM `product` WHERE id = '$id'");
-//  header("location.index.product.php");
+ header("location:index.product.php");
 //  window.location.href = 'index.product.php';
 ?>
