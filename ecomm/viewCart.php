90543d76470e16dc93908bdefefcc064f8934ada
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-02-20 20:53:27 +0545

    viewcart updated

--- ecomm/viewCart.php
+++ ecomm/viewCart.php
@@ -13,7 +13,8 @@
     
 </head>
 <body>
-<div class="table-container">
+<div style="display: flex; justify-content: space-between; align-items: flex-start;">
+<div class="table-container" style="width: 70%; margin-top: 40px; margin-left: 40px">
     <h1>My Cart</h1>
     <table border=0 cellpadding=4 ccellspacing=4>
         <thead>
@@ -80,7 +81,7 @@
 </div>
 
 
-<div>
+<div style="margin-top: 40px; margin-right: 20px">
     <h3>All Product Total</h3>
     <h1>
         <?php
@@ -89,7 +90,24 @@
     </h1>
 </div>
 
+   <!-- Checkout Section -->
+   <div style="width: 25%; padding: 20px; border: 1px solid #ccc; border-radius: 10px; background-color: #f8f8f8;  margin-right:40px; margin-top: 40px">
+        <h2>Checkout Summary</h2>
+        <p><strong>Total:</strong> NPR <?php echo number_format($Alltotal,2); ?></p>
 
-
+        <?php
+        echo    "
+                <form action='stripe/checkout.php' method='post'>
+                    <!-- <input class='btn' value='Buy Product' type='submit'> -->
+                    <input class='btn' value='$value[productName]' name = 'product_name' type='hidden'>
+                    <input class='btn' value='$value[productPrice]' name = 'product_price' type='hidden'>
+                    <input type='hidden' name='$value[productQuantity]' value=' ".(isset($_POST["productQuantity"]) ? $_POST["productQuantity"] : 1) ." '>
+            
+                 <button type='submit' style='padding: 10px 20px; background-color: #a0522d; color: white; border: none; border-radius: 5px; cursor: pointer;'>Proceed to Checkout</button>
+            </form>
+        ";
+        ?>
+    </div>
+</div>
 </body>
 </html>
